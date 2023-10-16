9b0b3dccaadccb091985eb48d00a6f523cf2fa2b
andrés <olivera.h.andres@gmail.com>
2023-10-16 12:48:09 -0300

    tp_movies

---
+++ src/controllers/genresController.js
@@ -0,0 +1,19 @@
+const db = require("../database/models");
+const sequelize = db.sequelize;
+
+const genresController = {
+  list: (req, res) => {
+    db.Genre.findAll({
+      include : ['movies']
+    }).then((genres) => {
+      res.render("genresList", { genres });
+    });
+  },
+  detail: (req, res) => {
+    db.Genre.findByPk(req.params.id).then((genre) => {
+      res.render("genresDetail", { genre });
+    });
+  },
+};
+
+module.exports = genresController;
